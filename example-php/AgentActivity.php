e590a6ae58db1041cb2edd977d3b5fed15c44f30
nstubbe <stubbe.niels@gmail.com>
2017-09-28 18:32:48 +0200

    Example PHP scripts to retrieve json from Zendesk API

---
+++ example-php/AgentActivity.php
@@ -0,0 +1,22 @@
+<?php
+//Quick PHP script so we don't have to deal with CORS
+$url = 'https://yourname.zendesk.com/api/v2/channels/voice/stats/agents_activity.json';
+$username = 'admin username';
+$password = 'admin password';
+$ch = curl_init();
+
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+
+// grab URL and pass it to the browser
+curl_exec($ch);
+
+// close cURL resource, and free up system resources
+curl_close($ch);
+
+
+
+
+
