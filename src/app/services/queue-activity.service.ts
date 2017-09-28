e7f0fc5bbace7dbd21432649f35aa8086d6c7ba8
nstubbe <stubbe.niels@gmail.com>
2017-09-28 16:36:14 +0200

    Added service to get queue activity

--- src/app/services/queue-activity.service.ts
+++ src/app/services/queue-activity.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import {queue} from '../queue';
 
 @Injectable()
 export class QueueActivityService {
 
-  constructor() { }
+    //URL to your backend which returns json from Zendesk (to avoid CORS)
+    private queueUrl = 'http://localhost/QueueActivity.php'
+    
+      constructor(private http: Http) {}
+    
+      getAgentActivityList(): Observable<queue>{
+        return this. http.get(this.queueUrl)
+        .map(res => res.json().current_queue_activity)
+        .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+      }
 
 }
